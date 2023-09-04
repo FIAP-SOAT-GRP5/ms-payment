73491ebc0bf0468fd4e06d7fd4d252ea01909a94
VIANNA-PC\rpvia <dev5@dixhealth.com.br>
2023-09-04 07:25:17 -0300

    Corrigido cadastro de cliente

--- src/framework/modules/client/client.module.ts
+++ src/framework/modules/client/client.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { buildClientController } from '../../../domain/factories/client/client.controller.factory';
 import { buildClientGateway } from '../../../domain/factories/client/client.gateway.factory';
+import { buildCreateClientUseCase } from '../../../domain/factories/client/create-client.use-case.factory';
 import { buildGetClientUseCase } from '../../../domain/factories/client/get-client.use-case.factory';
 import { CLIENT_CONTROLLER, CLIENT_GATEWAY, CREATE_CLIENT_USE_CASE, GET_CLIENT_USE_CASE } from '../../../domain/symbols/client.symbols';
 import { ClientEntity } from '../../entities/client.entity';
@@ -30,7 +31,7 @@ import { ClientRepository } from './client.repository';
 		{
 			provide: CREATE_CLIENT_USE_CASE,
 			inject: [CLIENT_GATEWAY],
-			useFactory: buildGetClientUseCase,
+			useFactory: buildCreateClientUseCase,
 		},
 	],
 	controllers: [ClientApi],
