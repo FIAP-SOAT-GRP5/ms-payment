4b49e23425b6917ec9d9d518a7788bd6737ad177
VIANNA-PC\rpvia <dev5@dixhealth.com.br>
2024-01-29 13:45:05 -0300

    Add conditional configuration for MercadoPago access token

--- src/config/mercadopago.ts
+++ src/config/mercadopago.ts
@@ -2,9 +2,10 @@
 import * as mercadopago from 'mercadopago';
 import env from './env';
 console.log('env.MP_ACCESS_TOKEN', env.MP_ACCESS_TOKEN)
-mercadopago.configure({
-	access_token: env.MP_ACCESS_TOKEN
-});
+if (env.MP_ACCESS_TOKEN)
+	mercadopago.configure({
+		access_token: env.MP_ACCESS_TOKEN
+	});
 
 export default mercadopago;
 /* v8 ignore stop */
